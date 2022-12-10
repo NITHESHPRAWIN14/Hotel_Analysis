d871f1995f7082d7093e4e3d8a5ad4167476f65a
NITHESHPRAWIN14 <67099475+NITHESHPRAWIN14@users.noreply.github.com>
2022-12-10 20:27:17 +0530

    submit button added

--- app.py
+++ app.py
@@ -9,25 +9,27 @@ from data import hotel_info
 
 
 if __name__ == '__main__':
-    st.markdown("<h1> Customer Status Predictor </h1>",unsafe_allow_html=True)
-    category = st.selectbox(
-            label="Hotel Category (Luxury/Business)",
-            options=('Luxury','Business')
-        )
+    st.header("Customer Status Predictor")
+    with st.form("Booking Form"):
+        category = st.selectbox(
+                label="Hotel Category (Luxury/Business)",
+                options=('Luxury','Business')
+            )
 
-    
-    st.session_state.cities = list(hotel_info['city'].keys())
-    city = st.selectbox(
-        label='City',
-        options= st.session_state.cities
-    )
-    
-    st.session_state.hotels = list(hotel_info['city'][city][category])
-    hotel = st.selectbox(
-        label='Hotel',
-        options=st.session_state.hotels
-    )
-    
-    check_in_date = st.date_input('Check In Date')
-    no_guest = st.number_input('Number of Guests',value=0)
-    days_stay = st.number_input('Days Staying',value=0)
+        
+        st.session_state.cities = list(hotel_info['city'].keys())
+        city = st.selectbox(
+            label='City',
+            options= st.session_state.cities
+        )
+        
+        st.session_state.hotels = list(hotel_info['city'][city][category])
+        hotel = st.selectbox(
+            label='Hotel',
+            options=st.session_state.hotels
+        )
+        
+        check_in_date = st.date_input('Check In Date')
+        no_guest = st.number_input('Number of Guests',value=0)
+        days_stay = st.number_input('Days Staying',value=0)
+        button = st.form_submit_button('Book Now')
